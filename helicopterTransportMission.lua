ca61484270006e5be78b6f7e3c2123e543ff08a7
Helio Leao <hmtleao@hotmail.com>
2024-01-23 00:30:53 -0300

    all landing zones mapped

--- helicopterTransportMission.lua
+++ helicopterTransportMission.lua
@@ -1,16 +1,45 @@
-local triggerZone = trigger.misc.getZone('hel-1')
-local playerPosition = Unit.getByName('blue'):getPoint()
+local ZONE_BASE_NAME = 'lz' -- lz-1, lz-2...
 
+function isUnitInsideZone(unit, zone)
+    local unitPosition = unit:getPoint()
 
-function isUnitInsideTriggerZone()
-    trigger.action.outText('zone\n' .. 'X: ' .. triggerZone.point.x .. ' Z: ' .. triggerZone.point.z
-    .. ' alt: ' .. triggerZone.point.y .. ' radius: ' .. triggerZone.radius, 1)
+    local result = unitPosition.x >= (zone.point.x - zone.radius)
+        and unitPosition.x <= (zone.point.x + zone.radius)
+        and unitPosition.y >= (zone.point.y - zone.radius)
+        and unitPosition.y <= (zone.point.y + zone.radius)
 
-    trigger.action.outText('player\n' .. 'X: ' .. playerPosition.x .. ' Z: ' .. playerPosition.z
-        .. ' alt: ' .. playerPosition.y, 1)
+    trigger.action.outText('zone'
+        .. '\nX: ' .. zone.point.x .. ' Z: ' .. zone.point.z
+        .. ' alt: ' .. zone.point.y .. ' radius: ' .. zone.radius
+        .. '\n\nplayer'
+        .. '\nX: ' .. unitPosition.x .. ' Z: ' .. unitPosition.z
+        .. ' alt: ' .. unitPosition.y
+        .. '\n\nunit inside zone: ' .. (result and 'true' or 'false')
+    , 1)
 
-    return playerPosition.x >= (triggerZone.point.x - triggerZone.radius)
-        and playerPosition.x <= (triggerZone.point.x + triggerZone.radius)
-        and playerPosition.y >= (triggerZone.point.y - triggerZone.radius)
-        and playerPosition.y <= (triggerZone.point.y + triggerZone.radius)
+    return result
 end
+
+function getAllLandingZones()
+    local allLandingZones = {}
+    local index = 1
+    local landingZone = trigger.misc.getZone(ZONE_BASE_NAME .. '-' .. index)
+
+    while landingZone do
+        table.insert(allLandingZones, landingZone)
+        index = index + 1
+        landingZone = trigger.misc.getZone(ZONE_BASE_NAME .. '-' .. index)
+    end
+
+    return allLandingZones
+end
+
+function main()
+    local allLandingZones = getAllLandingZones()
+    trigger.action.outText('landing zones count: ' .. #allLandingZones, 1)
+
+    -- isUnitInsideZone(Unit.getByName('blue'),
+    --     trigger.misc.getZone('hel-1'))
+end
+
+main()
